243e17f4f4c2aa28e87f2763a37e47dbcca5f79a
tanjo <tanjo@users.noreply.github.com>
2026-03-30 22:07:51 +0900

    パス修正

--- メスガキと学ぶGitHub入門講座/develop/vite.config.js
+++ メスガキと学ぶGitHub入門講座/develop/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/slides/%E3%83%A1%E3%82%B9%E3%82%AC%E3%82%AD%E3%81%A8%E5%AD%A6%E3%81%B6GitHub%E5%85%A5%E9%96%80%E8%AC%9B%E5%BA%A7/docs/',
   plugins: [react()],
   build: {
     outDir: '../docs',
